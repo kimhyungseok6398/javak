8a021105a122c4dfa844067193530565b5f7395b
kimhyungseok6398 <gudtjr6398@naver.com>
2023-03-29 06:29:15 +0900

    자바 2일

---
+++ Java1/src/condition/If1.java
@@ -0,0 +1,13 @@
+package condition;
+
+public class If1 {
+	public static void main(String[] args) {
+		 System.out.println("조건이 맞았습니다");
+		 System.out.println("실행");
+		 
+	}
+	int num = 9;    //외부에서 주어진 값
+	if(num < 10) {	//들어온 값을 통해 코드 실행여뷰 결정	
+		System.out.println("tntwksms 10보다 작습니다");
+	}
+}
