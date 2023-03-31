e8d828a548ac70c92cfc5490678cffeeba2c23ea
kimhyungseok6398 <gudtjr6398@naver.com>
2023-04-01 06:46:50 +0900

    자바4일

--- JAVA04/src/Away/ArrArr.java
+++ JAVA04/src/Away/ArrArr.java
@@ -1,7 +1,9 @@
 package Away;
 
-public class Arrarr {
+public class ArrArr {
 	public static void main(String[] args) {
+		
+		
 		//2차원 배열
 		//배열을 전체 사용(출력)할려면 반복믄을 사용
 		//2차원 배열을 전체사용할려면 이중 반복문을 사용
--- JAVA04/src/Away/arr4.java
+++ JAVA04/src/Away/arr4.java
@@ -1,5 +1,7 @@
 package Away;
 
+import java.util.Scanner;
+
 public class arr4 {
 	public static void main(String[] args) {
 		 // 국가와 수도 수도 (5개)
@@ -15,20 +17,30 @@ public class arr4 {
 		 * 
 		 * 
 		 */
+		//사용저앱력으로 국가를 받아서 
+		//해당국가 수도룰 알려주는 프로그램을 만들어라
 		String names[] = {"대한민국","일본","중국","미국","러시아"};
 		String city[] = {"서울","도쿄","베이징","워싱턴","모스크바"};
 		for(int i=0;i<names.length; ++i) {
-			System.out.println(names[i]+"-"+city[i]);
-			
-			
+			System.out.println(names[i]+"의 수도는 "+city[i]+"입니다");
+		}
 	
+			Scanner sc = new Scanner(System.in);
+			string user_in
+			put = "";
+			System.out.print("수도를 알고자 하는 국가의 이름을 입력하세요>>");
+			user_input = sc.next();
+			
+			//해당하는 위치를 찾는다
+			
+			
 				
-		
-		 
-		
-		//
 		
 	}
 
-	
+		
+		
+	}
+
+	}
 }
