184eb7e3196ba93a998fe0bebfcb16f1f3143ef7
kimhyungseok6398 <gudtjr6398@naver.com>
2023-04-11 00:10:21 +0900

    튜토02

---
+++ 튜토/src/튜토01/If.java
@@ -0,0 +1,10 @@
+package 튜토01;
+
+public class If {
+	public static void main(String[] args) {
+		if(true){
+            System.out.println("result : true");
+        }
+	}
+
+}
---
+++ 튜토/src/튜토01/If3.java
@@ -0,0 +1,12 @@
+package 튜토01;
+
+public class If3 {
+	public static void main(String[] args) {
+		if (true) {
+            System.out.println(1);
+        } else {
+            System.out.println(2);
+        }
+	}
+
+}
